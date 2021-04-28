e83f73ff6ce0bafe04fa0f9e16375e5a61941283
Marco F <m.faverof@gmail.com>
2021-04-28 15:30:29 +0200

    update tests

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -2,12 +2,12 @@
 
 function travis_test {
     local ARGUMENTS=
-    echo -en "=======\n\n\e[96m$@\e[0m\n\n" >> $LDIR/test.log
     PROGNAME="$1"
     PROG=$2
     for (( i=3; i <= "$#"; i++ )); do
         ARGUMENTS="$ARGUMENTS ${!i}"
     done
+    echo -en "=======\n\n\e[96m$PROGNAME\e[0m $PROG $ARGUMENTS\n\n" >> $LDIR/test.log
     echo $ARGUMENTS | xargs $PROG >> $LDIR/test.log 2>&1
     local status=$?
     if [ $status -ne 0 ]; then
@@ -24,7 +24,8 @@ function travis_test {
 LDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 > $LDIR/test.log
 status=0
-while read  test || [ -n "$test" ]; do
+IFS=";"
+while read test || [ -n "$test" ]; do
 	travis_test $test
 	status=$(($status + $?))
 done < $LDIR/TESTLIST
